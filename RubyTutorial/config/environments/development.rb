d84514cd912200216987f6f1fefc92e702e92b69
Yunying <yunyingt@usc.edu>
2015-06-28 11:42:42 -0700

    syntax error

--- RubyTutorial/config/environments/development.rb
+++ RubyTutorial/config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com'
+    domain:               'gmail.com',
     user_name:            'yunyingtu@gmail.com',
     password:             'shumybest',
     authentication:       'plain',
